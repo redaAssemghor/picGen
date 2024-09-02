a58cfc1e0824a89e6cfed75226563ccbd0ad0d4d
redaAssemghor <assemghor.reda@gmail.com>
2024-09-02 15:53:46 +0100

    deploy bug

--- app/api/createUser/route.ts
+++ app/api/createUser/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "@/prisma";
 
 export async function POST(req: NextRequest) {
   const { userId } = getAuth(req);
--- app/api/points/route.ts
+++ app/api/points/route.ts
@@ -1,8 +1,6 @@
 import { getAuth } from "@clerk/nextjs/server";
-import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
+import prisma from "@/prisma";
 
 export async function GET(req: NextRequest) {
   const { userId } = getAuth(req);
