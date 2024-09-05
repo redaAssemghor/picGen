dd253f1317c64db3923d3b28e6c1dd145799d08d
redaAssemghor <assemghor.reda@gmail.com>
2024-09-05 19:28:36 +0100

    prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -4,12 +4,13 @@ const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// Extend globalThis to include the custom property for Prisma client
+declare global {
+  var prismaGlobal: PrismaClient | undefined;
+}
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
